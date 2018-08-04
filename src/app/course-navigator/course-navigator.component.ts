ab8a9a9a8d61af1d03fd070e9c95363152741360
Melina <sayeghmelina@gmail.com>
2018-08-04 17:32:35 -0400

    moved bootstrap

--- src/app/course-navigator/course-navigator.component.ts
+++ src/app/course-navigator/course-navigator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CourseNavigatorServiceClient} from '../services/CourseNavigatorServiceClient';
 
 @Component({
   selector: 'app-course-navigator',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseNavigatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: CourseNavigatorServiceClient) { }
+  courses = [];
+  modules = [];
+  lessons = [];
+  widgets = [];
 
   ngOnInit() {
-  }
+    this.service.findAllCourses()
+      .then(courses => this.courses = courses);
 
+    this.service.findAllModules()
+      .then(modules => this.modules = modules);
+
+    this.service.findAllLessons()
+      .then(lessons => this.lessons = lessons);
+
+    this.service.findAllWidgets()
+      .then(widgets => this.widgets = widgets);
+  }
 }
--- src/app/services/CourseNavigatorServiceClient.ts
+++ src/app/services/CourseNavigatorServiceClient.ts
@@ -1,8 +1,92 @@
+const COURSE_API_URL = 'https://webdev-server-java-melina.herokuapp.com/api/course';
+
+const MODULE_API_URL = 'https://webdev-server-java-melina.herokuapp.com/api/module';
+const COURSE_MODULE_API_URL = 'https://webdev-server-java-melina.herokuapp.com/api/course/CID/module';
+
+const LESSON_API_URL = 'https://webdev-server-java-melina.herokuapp.com/api/lesson';
+const C_M_L_API_URL = 'https://webdev-server-java-melina.herokuapp.com/api/course/CID/module/MID/lesson';
+
+const WIDGET_API_URL = 'https://webdev-server-java-melina.herokuapp.com/api/widget';
+const W_L_API_URL = 'https://webdev-server-java-melina.herokuapp.com/api/lesson/LID/widget';
+
 export class CourseNavigatorServiceClient {
 
-  findAllCourses() {
-    return fetch
-    ('http://localhost:8080/api/course')
+  // COURSES
+  // find course by id
+  findCourseById(courseId) {
+    return fetch(COURSE_API_URL + courseId)
       .then(response => response.json());
+  }
+
+  findAllCourses() {
+    return fetch(COURSE_API_URL)
+      .then(response => response.json());
+  }
+
+  // MODULES
+  findModuleById(moduleId) {
+    return fetch( MODULE_API_URL +  moduleId, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => response.json());
+  }
+
+  findAllModules() {
+    return fetch(MODULE_API_URL)
+      .then(response => response.json());
+  }
+
+  findAllModulesForCourse(courseId) {
+    return fetch(COURSE_MODULE_API_URL.replace('CID', courseId), {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => response.json());
+  }
+
+  // LESSONS
+  findLessonById(lessonId) {
+    return fetch( LESSON_API_URL + '/' +  lessonId, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    }).then(function (response) {
+      return response.json();
+    });
+  }
+
+  findAllLessonsForModule(courseId, moduleId) {
+    return fetch(C_M_L_API_URL.replace('CID', courseId).replace('MID', moduleId), {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    }).then(function (response) {
+      return response.json();
+    });
+  }
+
+  findAllLessons() {
+    return fetch(LESSON_API_URL)
+      .then(response => response.json());
+  }
+
+  findAllWidgets() {
+    return fetch(WIDGET_API_URL)
+      .then(response => response.json());
+  }
+
+  findWidgetById(widgetId) {
+    return fetch( WIDGET_API_URL + '/' +  widgetId, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    }).then(function (response) {
+      return response.json();
+    });
+  }
+
+  findAllWidgetsForLesson(lessonId) {
+    return fetch(W_L_API_URL.replace('LID', lessonId), {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    }).then(function (response) {
+      return response.json();
+    });
   }
 }
