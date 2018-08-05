16089db903302a9f28bdbf54cb9cd292ceddd9a3
Melina <sayeghmelina@gmail.com>
2018-08-05 00:27:24 -0400

    generated enrollment, login, profile, register, section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { WhiteboardComponent } from './whiteboard/whiteboard.component';
 import { CourseNavigatorServiceClient } from './services/coursenavigator.service.client';
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
 import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { EnrollmentComponent } from './enrollment/enrollment.component';
+import { SectionComponent } from './section/section.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WhiteboardComponent,
     CourseNavigatorComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegisterComponent,
+    LoginComponent,
+    EnrollmentComponent,
+    SectionComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
