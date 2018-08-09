cfc99b36b808d6523a610660cd5cb44efc0b2776
Melina <sayeghmelina@gmail.com>
2018-08-09 16:30:46 -0400

    fix for heroku

--- src/app/server.js
+++ src/app/server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/webdev-summer2-client-angular-melina'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/webdev-summer2-client-angular-melina//index.html'));
+  res.sendFile(path.join(__dirname,'/webdev-summer2-client-angular-melina/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
